76b3141ead9a20e43e11b43e8280da5013830f3e
Al-Hussein <Hussein.Salah8696@hotmail.com>
2020-09-12 09:58:00 +0200

    editPassword, editAccount, myAccount ui

---
+++ src/screens/Account/EditAccount/index.js
@@ -0,0 +1,94 @@
+import React, {useState, useRef} from 'react';
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Platform,
+  Dimensions,
+} from 'react-native';
+import IconFeather from 'react-native-vector-icons/Feather';
+import IconIonicons from 'react-native-vector-icons/Ionicons';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import {SceneMap, TabBar, TabView} from 'react-native-tab-view';
+import styles from './styles';
+import COLORS from '../../../common/colors';
+import AppText from '../../../components/atoms/AppText';
+import DropDown from '../../../components/atoms/DropDown';
+import Button from '../../../components/atoms/Button';
+import ImagesSlider from '../../../components/atoms/ImageSlider';
+import IMAGES from '../../../common/images';
+import {calcHeight, calcWidth, calcFont} from '../../../common/styles';
+import {Line} from '../../../components/atoms/Line';
+import CheckBox from '../../../components/atoms/CheckBox';
+
+const EditAccount = () => {
+  const [value, onChangeText] = useState('');
+  return (
+    <ScrollView style={{backgroundColor: COLORS.white}}>
+      <View style={styles.container}>
+        <View style={styles.newOrder}>
+          <AppText style={styles.newOrderText}>حسابي</AppText>
+          <Button
+            title={'حفظ'}
+            onPress={() => console.log('pressed')}
+            titleStyle={styles.saveText}
+            style={styles.saveButton}
+          />
+        </View>
+
+        <View style={styles.userOut}>
+          <Image source={IMAGES.userImage} style={styles.userImage} />
+          <Button
+          title={'حمل الصوره'}
+          onPress={() => console.log('pressed')}
+          titleStyle={styles.addToCartText}
+          style={styles.addToCartButton}
+        />
+        </View>
+
+        <View style={styles.data}>
+          <AppText style={styles.titleText}>اسم المستخدم</AppText>
+          <View style={styles.promoCode}>
+            <TextInput
+              style={styles.promoCodeInput}
+              onChangeText={(text) => onChangeText(text)}
+              value={value}
+              placeholder="Mahmoud Mohamed"
+              placeholderTextColor={COLORS.lightTextGray}
+            />
+          </View>
+        </View>
+
+        <View style={styles.data}>
+          <AppText style={styles.titleText}>الهاتف</AppText>
+          <View style={styles.promoCode}>
+            <TextInput
+              style={styles.promoCodeInput}
+              onChangeText={(text) => onChangeText(text)}
+              value={value}
+              placeholder="01112563214"
+              placeholderTextColor={COLORS.lightTextGray}
+            />
+          </View>
+        </View>
+
+        <View style={styles.data}>
+          <AppText style={styles.titleText}>البريد الالكترونى</AppText>
+          <View style={styles.promoCode}>
+            <TextInput
+              style={styles.promoCodeInput}
+              onChangeText={(text) => onChangeText(text)}
+              value={value}
+              placeholder="Mahmoud@gmmm.com"
+              placeholderTextColor={COLORS.lightTextGray}
+            />
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default EditAccount;
---
+++ src/screens/Account/EditAccount/styles.js
@@ -0,0 +1,93 @@
+import {StyleSheet, CheckBox} from 'react-native';
+import COLORS from '../../../common/colors';
+import {calcHeight, calcWidth, calcFont} from '../../../common/styles';
+import {color} from 'react-native-reanimated';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    alignItems: 'center',
+  },
+
+  newOrder: {
+    width: '100%',
+    height: calcHeight(54),
+    backgroundColor: COLORS.lightGray,
+    justifyContent: 'space-between',
+    flexDirection: 'row-reverse',
+    paddingHorizontal: calcWidth(15),
+    alignItems: 'center',
+  },
+  newOrderText: {
+    color: COLORS.mainText,
+    fontSize: calcFont(20),
+    fontWeight: 'bold',
+    color: COLORS.gray,
+  },
+  saveText: {
+    width: '100%',
+    color: COLORS.mainText,
+    fontSize: calcFont(15),
+  },
+  saveButton: {
+    width: calcWidth(75),
+    height: calcHeight(30),
+    borderRadius: calcFont(15),
+    backgroundColor: COLORS.yallow,
+  },
+  userImage: {
+    width: calcWidth(75),
+    height: calcHeight(75),
+    resizeMode: 'contain',
+  },
+  userOut: {
+    width: calcWidth(134),
+    height: calcWidth(134),
+    backgroundColor: COLORS.lightGray,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: calcWidth(Platform.OS === 'ios' ? 134 : 72),
+    borderColor: COLORS.midLightGray,
+    borderWidth: calcHeight(6),
+    marginVertical: calcHeight(15),
+  },
+  addToCartText: {
+    width: '100%',
+    color: COLORS.white,
+    fontSize: calcFont(18),
+  },
+  addToCartButton: {
+    width: calcWidth(124),
+    height: calcHeight(42),
+    borderRadius: calcFont(5),
+    backgroundColor: '#00000088',
+    position:'absolute'
+  },
+  titleText: {
+    color: COLORS.textGray,
+    fontSize: calcFont(18),
+    fontWeight: 'bold',
+    marginVertical: calcHeight(5),
+  },
+  promoCode: {
+    width: calcWidth(345),
+    height: calcHeight(46),
+    borderRadius: calcFont(3),
+    backgroundColor: COLORS.white,
+    borderColor: COLORS.midLightGray,
+    borderWidth: calcHeight(2),
+    alignItems: 'center',
+  },
+  promoCodeInput: {
+    width: calcWidth(330),
+    textAlign:'right'
+  },
+  //////////////////////////////////////////
+  data: {
+    paddingBottom: calcHeight(10),
+    width: '100%',
+    paddingHorizontal: calcWidth(15),
+  },
+});
