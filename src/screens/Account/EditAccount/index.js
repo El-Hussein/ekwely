ed9d7963e7804acd9ca564539c4598c43829f4d2
Ebrahim Elngary <ebrahim@new-vision.agency>
2020-11-02 23:51:53 +0200

    fix

--- src/screens/Account/EditAccount/index.js
+++ src/screens/Account/EditAccount/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Image,
@@ -12,13 +12,13 @@ import COLORS from '../../../common/colors';
 import AppText from '../../../components/atoms/AppText';
 import Button from '../../../components/atoms/Button';
 import IMAGES from '../../../common/images';
-import {calcWidth, calcFont} from '../../../common/styles';
-import {useNavigation} from '@react-navigation/native';
-import {makePostRequest, API_BASE_URL} from '../../../utils/api.helpers';
-import {useDispatch, useSelector} from 'react-redux';
+import { calcWidth, calcFont } from '../../../common/styles';
+import { useNavigation } from '@react-navigation/native';
+import { makePostRequest, API_BASE_URL } from '../../../utils/api.helpers';
+import { useDispatch, useSelector } from 'react-redux';
 import Toast from 'react-native-simple-toast';
 import AppInput from '../../../components/atoms/AppInput';
-import {IMAGE_BASE_URL, USER_DATA} from '../../../common/constants';
+import { IMAGE_BASE_URL, USER_DATA } from '../../../common/constants';
 
 import {
   validateUserName,
@@ -26,9 +26,9 @@ import {
   validatePhone,
 } from '../../../common/Validation';
 import AsyncStorage from '@react-native-community/async-storage';
-import {SIGN_IN} from '../../../redux/actions/types';
+import { SIGN_IN } from '../../../redux/actions/types';
 const EditAccount = () => {
-  const {user} = useSelector((state) => {
+  const { user } = useSelector((state) => {
     return {
       user: state.auth.user,
     };
@@ -48,7 +48,7 @@ const EditAccount = () => {
   const [userNameError, setUserNameError] = useState('');
   const [phoneError, setPhoneError] = useState('');
   const [userImage, setUserImage] = useState(
-    user?.image ? {uri: IMAGE_BASE_URL + user?.image} : IMAGES.userImage,
+    user?.image ? { uri: IMAGE_BASE_URL + user?.image } : IMAGES.userImage,
   );
 
   const _validate = () => {
@@ -124,12 +124,12 @@ const EditAccount = () => {
       } else if (response.error) {
       } else if (response.customButton) {
       } else {
-        setUserImage({uri: response.uri});
+        setUserImage({ uri: response.uri });
         upload(response).then((response) => {
           setImageLoading(false);
           if (response.status === '200') {
-            setEditData({...editData, Image: response.data});
-            setUserImage({uri: IMAGE_BASE_URL + response.data});
+            setEditData({ ...editData, Image: response.data });
+            setUserImage({ uri: IMAGE_BASE_URL + response.data });
           } else {
             Toast.show('حدث خطأ ما من فضلك حاول مرة اخري');
           }
@@ -141,7 +141,7 @@ const EditAccount = () => {
 
   const upload = async (imageObj) => {
     try {
-      var ret = await fetch(`${API_BASE_URL}UploadDownload/upload`, {
+      var ret = await fetch(API_BASE_URL + 'UploadDownload/upload', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
@@ -170,20 +170,20 @@ const EditAccount = () => {
   };
 
   return (
-    <ScrollView style={{backgroundColor: COLORS.white}}>
+    <ScrollView style={{ backgroundColor: COLORS.white }}>
       <View style={styles.container}>
         <View style={styles.newOrder}>
           <AppText style={styles.newOrderText}>حسابي</AppText>
           {loading ? (
             <ActivityIndicator color={COLORS.main} size={calcFont(20)} />
           ) : (
-            <Button
-              title={'حفظ'}
-              onPress={() => saveData()}
-              titleStyle={styles.saveText}
-              style={styles.saveButton}
-            />
-          )}
+              <Button
+                title={'حفظ'}
+                onPress={() => saveData()}
+                titleStyle={styles.saveText}
+                style={styles.saveButton}
+              />
+            )}
         </View>
 
         <View style={styles.userOut}>
@@ -201,13 +201,13 @@ const EditAccount = () => {
               }}
             />
           ) : (
-            <Button
-              title={'حمل الصوره'}
-              onPress={openGallery}
-              titleStyle={styles.addToCartText}
-              style={styles.addToCartButton}
-            />
-          )}
+              <Button
+                title={'حمل الصوره'}
+                onPress={openGallery}
+                titleStyle={styles.addToCartText}
+                style={styles.addToCartButton}
+              />
+            )}
         </View>
 
         {/* <View style={styles.data}>
@@ -235,7 +235,7 @@ const EditAccount = () => {
             error={userNameError}
             value={editData.userName}
             onChangeText={(userName) => {
-              setEditData({...editData, userName});
+              setEditData({ ...editData, userName });
             }}
             onEndEditing={() => {
               setUserNameError(validateUserName(editData.userName));
@@ -251,7 +251,7 @@ const EditAccount = () => {
             inputStyle={styles.promoCodeInput}
             value={editData.phone}
             onChangeText={(phone) => {
-              setEditData({...editData, phone});
+              setEditData({ ...editData, phone });
             }}
             keyboardType="numeric"
             onEndEditing={() => {
